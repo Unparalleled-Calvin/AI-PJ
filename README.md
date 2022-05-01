7b6bbc593a5ad4736749942b65990d04b8d558b3
Calvin <1176479962@qq.com>
2022-05-01 15:54:17 +0800

    前端编写完毕

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 #### 项目规划
 
 根据用户房源需求，利用模糊推理以及杨浦区POI数据以获得最佳房源推荐
+
+#### 参考资料
+
+- 前端样式: [jotform](https://www.jotform.com/form-templates/survey/employee-surveys)
+- 系统设计: [fuzzy_expert_system](https://github.com/WxxShirley/fuzzy_expert_system)
+- 数据来源: [地学大数据](https://mp.weixin.qq.com/s/zYwF3a8RI-APVT7sMXp5uQ)
--- dwelling/webpage/views.py
+++ dwelling/webpage/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from urllib.parse import unquote
 
 # Create your views here.
 def servey(request):
+    if request.method == "POST":
+        data = parse_data(unquote(request.body.decode()))
     return render(request, "servey.html")
+
+def parse_data(data_str: str):
+    data = {}
+    for pair in data_str.split("&"):
+        k,v = pair.split("=")
+        data[k] = v
+    return data
